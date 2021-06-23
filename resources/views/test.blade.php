c695375e1a846505e050b727f86864137722c4f2
BernieLiege <bernard.jacob.liege@gmail.com>
2021-06-23 13:42:00 +0200

    test menu horiz

---
+++ resources/views/test.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+  	<title>Test Menu horiz bootstrap 5</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+	<section class="ftco-section">
+
+		<div class="container">
+			<nav class="navbar navbar-expand-lg ftco_navbar ftco-navbar-light" id="ftco-navbar">
+		    <div class="container">
+		    	<a class="navbar-brand" href="index.html">Digital</a>
+		    	<div class="social-media order-lg-last">
+		    		<p class="mb-0 d-flex">
+		    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+		    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+		    		</p>
+	        </div>
+		      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+		        <span class="fa fa-bars"></span> Menu
+		      </button>
+		      <div class="collapse navbar-collapse" id="ftco-nav">
+		        <ul class="navbar-nav ml-auto mr-md-3">
+		        	<li class="nav-item active"><a href="#" class="nav-link">Accueil</a></li>
+		        	<li class="nav-item"><a href="#" class="nav-link">Véhicule</a></li>
+		        	<li class="nav-item"><a href="#" class="nav-link">Sinistre</a></li>
+		        	<li class="nav-item"><a href="#" class="nav-link">Personnel</a></li>
+		            <li class="nav-item"><a href="#" class="nav-link">Suivi</a></li>
+		        </ul>
+		      </div>
+		    </div>
+		  </nav>
+        </div>
+    <!-- END nav -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 text-center mb-5">
+                <p>Bla bal balaaaaa Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Adipisci officiis corporis aut assumenda necessitatibus a eligendi numquam laudantium at.
+                    Tempore obcaecati nobis totam quam dolore error culpa temporibus sint quos.</p>
+                    bonjour kakapo !
+            </div>
+        </div>
+    </div>
+
+	</section>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+</body>
+</html>
